432a03fc121ed1c6e452332e21fd163c377ab80d
Lautaro <lauta010101@gmail.com>
2023-02-15 14:04:44 -0300

    Realizada maquetacion para moviles

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { AboutComponent } from './app/pages/about/about.component';
 import { PortfolioComponent } from './app/pages/portfolio/portfolio.component';
 
 const routes: Routes = [
-  { path: ''     , component: HomeComponent },
+  { path: 'home'     , component: HomeComponent },
   { path: 'about'    , component: AboutComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
 ]
 
 bootstrapApplication(AppComponent, { providers: [provideRouter(routes)]});
